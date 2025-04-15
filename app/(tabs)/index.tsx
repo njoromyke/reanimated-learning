45a45516a03a4107e77934d1ce28a0489f1dfa8f
njoromyke <mykenjoroge01@gmail.co>
2025-04-15 20:08:16 +0300

    using animated props

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,38 +5,27 @@ import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { SafeAreaView } from "react-native-safe-area-context";
-import Animated, { useAnimatedStyle, useSharedValue, withSpring } from "react-native-reanimated";
+import Animated, { useAnimatedProps, useAnimatedStyle, useSharedValue, withSpring, withTiming } from "react-native-reanimated";
+import Svg, { Circle } from "react-native-svg";
 
+const AnimatedCircle = Animated.createAnimatedComponent(Circle);
 export default function HomeScreen() {
-  const width = useSharedValue(100);
-  const translateX = useSharedValue<number>(0);
+  const r = useSharedValue(10);
 
-  const handleOnPress = () => {
-    translateX.value = translateX.value += 50;
+  const handlePress = () => {
+    r.value += 10;
   };
 
-  const animatedStyles = useAnimatedStyle(() => ({
-    transform: [
-      {
-        translateX: withSpring(translateX.value * 2),
-      },
-    ],
+  const animatedProps = useAnimatedProps(() => ({
+    r: withTiming(r.value),
   }));
 
   return (
     <SafeAreaView style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Animated.View
-        style={[
-          {
-            width: 300,
-            height: 100,
-            backgroundColor: "violet",
-          },
-          animatedStyles,
-        ]}
-      />
-
-      <Button onPress={handleOnPress} title="Press me" />
+      <Svg style={styles.svg}>
+        <AnimatedCircle cx="50%" cy="50%" fill="#b58df1" animatedProps={animatedProps} />
+      </Svg>{" "}
+      <Button onPress={handlePress} title="Click me" />
     </SafeAreaView>
   );
 }
@@ -57,5 +46,9 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     position: "absolute",
+  },
+  svg: {
+    height: 250,
+    width: "100%",
   },
 });
